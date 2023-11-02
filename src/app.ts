afd21e8472f7085c89030241656216d9755e08ab
Kyle Lastimosa <glitchkyle16@gmail.com>
2023-11-01 22:17:12 -0400

    Add needed middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "reflect-metadata";
+import { rateLimit } from "express-rate-limit";
 import { auth, requiresAuth } from "express-openid-connect";
-import Logger from "./config/logger";
 
-dotenv.config();
+import Logger from "./config/logger";
+import Morgan from "./config/morgan";
 
 const app = express();
 const port = process.env.PORT;
@@ -19,6 +23,30 @@ const config = {
 };
 
 app.use(auth(config));
+
+// Enable CORS
+app.use(cors());
+
+// Body Parser
+app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Security Headers
+app.use(helmet());
+
+// Morgan Logger
+app.use(Morgan);
+
+// Rate Limiting
+app.use(
+    rateLimit({
+        windowMs: 5 * 60 * 1000,
+        max: 100,
+        message: "Too many requests from this IP, please try again later",
+    })
+);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + Typescript Server");
